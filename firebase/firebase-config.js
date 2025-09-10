736b6a0ba8b4f1655e70643b63d76057f397291e
Chala-ni <hansikachalani875@gmail.com>
2025-09-10 16:37:39 +0530

    update

---
+++ firebase/firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB_89aNB9VU1XrcZEgyEV85gyh0-eJOBuE",
+    authDomain: "natura-52356.firebaseapp.com",
+    projectId: "natura-52356",
+    storageBucket: "natura-52356.firebasestorage.app",
+    messagingSenderId: "686103761624",
+    appId: "1:686103761624:web:52943036daac91d2c5bdad",
+    measurementId: "G-PPLV45WH6P"
+};
+console.log('Firebase config loaded:', firebaseConfig);
